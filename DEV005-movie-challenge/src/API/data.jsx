d87b38ae18bccd79ba9ab3f280f447e5a57b7966
angelica-rjs <angelicarojas.rys@gmail.com>
2023-07-25 19:26:11 -0400

    comenzando el despliege

--- DEV005-movie-challenge/src/API/data.jsx
+++ DEV005-movie-challenge/src/API/data.jsx
@@ -1,9 +1,12 @@
+const apyToken = import.meta.env.VITE_REACT_API_TOKEN;
+
+
 export async function GetData(url) {
   const response = await fetch(url, {
     method: 'GET',
     headers: {
       accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwODNmY2UwZWEyZjYwYzRjN2IzZmRjOWI5YWJlNGVkMyIsInN1YiI6IjY0YTVhN2E3OGM0NGI5MDEwYzlkMmQ0OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.mSUspfJvX2fKXK_J8PRtLEsrm-dmaiOJaq2czy3WDxc'
+      Authorization: apyToken
     }
   });
   return await response.json();
--- DEV005-movie-challenge/vite.config.js
+++ DEV005-movie-challenge/vite.config.js
@@ -1,13 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  css: {
-    modules: {
-      localsConvention: 'camelCaseOnly'
-    }
+
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
+  };
+
+  if (command !== 'serve') {
+    config.base = '/DEV005-movie-challenge-fw/';
   }
-})
 
+  return config;
+});
